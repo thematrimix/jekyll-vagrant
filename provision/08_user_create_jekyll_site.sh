2d76e8198699314143a8d90605bf4f6e8d616f83
Joshua Vaughn <thematrimix@yahoo.com>
2014-08-29 15:22:53 -0400

    Update 08_user_create_jekyll_site.sh

--- provision/08_user_create_jekyll_site.sh
+++ provision/08_user_create_jekyll_site.sh
@@ -2,7 +2,6 @@
 
 # $1 is the name of the site which comes from the arguments list
 
-#Now that we have the site setup with nginx lets have jekyll create us a starter
 type jekyll >/dev/null 2>&1
 
 if [ $? -ne 0 ] && (( $# )); then
@@ -11,15 +10,9 @@ if [ $? -ne 0 ] && (( $# )); then
 		cd /vagrant
 		jekyll new $1
 		cd ~
-		#now for the tricky thing
-		#jekyll by default adds a gitignore at the top level specificity to ignore the _site directory
-		#so delete it as we need to keep _site around even if we check it in say to github
-		rm -f /vagrant/$1/.gitignore
 	else
 		echo ">>>>>Site already exists skipping<<<<<"
 	fi
-	
-	#jekyll build --watch
 else
 	echo ">>>>>jekyll does not appear to be installed<<<<<"
 fi
